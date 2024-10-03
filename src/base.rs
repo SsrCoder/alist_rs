0ba4032934d3d7a4cb747c14ff01371c86962770
SsrCoder <SsrCoder@gmail.com>
2024-10-03 21:35:18 +0800

    feat(api): add get_file

--- src/base.rs
+++ src/base.rs
@@ -1,8 +1,13 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct Response<T> {
     pub code: u32,
     pub message: String,
     pub data: Option<T>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HashInfo {
+    pub sha1: Option<String>,
 }
--- src/client/fs.rs
+++ src/client/fs.rs
@@ -1,7 +1,11 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-use crate::{base::Response, client::Client, errors::Error};
+use crate::{
+    base::{HashInfo, Response},
+    client::Client,
+    errors::Error,
+};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FileList {
@@ -18,14 +22,19 @@ pub struct File {
     pub name: String,
     pub size: u64,
     pub is_dir: bool,
-    // pub hash_info: Option<String>,
+    pub hash_info: Option<HashInfo>,
     pub hashinfo: String,
+    pub header: Option<String>,
     pub sign: String,
     pub thumb: String,
     #[serde(rename = "type")]
     pub type_: u8,
     pub created: DateTime<Utc>,
     pub modified: DateTime<Utc>,
+    pub provider: Option<String>,
+    pub raw_url: Option<String>,
+    pub readme: Option<String>,
+    // pub related: Option<String?>,
 }
 
 #[derive(Debug, Serialize)]
@@ -102,7 +111,44 @@ pub struct Task {
     pub status: String,
 }
 
+#[derive(Debug, Serialize)]
+pub struct GetFileRequest {
+    pub path: String,
+    pub password: String,
+}
+
+impl Default for GetFileRequest {
+    fn default() -> Self {
+        Self {
+            path: "/".to_string(),
+            password: "".to_string(),
+        }
+    }
+}
+
+impl From<&str> for GetFileRequest {
+    fn from(path: &str) -> Self {
+        Self {
+            path: path.to_string(),
+            ..Default::default()
+        }
+    }
+}
+
 impl Client {
+    pub async fn get_file(&self, req: GetFileRequest) -> Result<File, Error> {
+        let resp: Response<File> = self
+            .client
+            .post(format!("{}/api/fs/get", self.host))
+            .header("Authorization", self.token.as_deref().unwrap_or_default())
+            .json(&req)
+            .send()
+            .await?
+            .json()
+            .await?;
+        resp.data.ok_or(Error::Message(resp.message))
+    }
+
     pub async fn list(&self, req: ListRequest) -> Result<FileList, Error> {
         // let resp = self
         //     .client
